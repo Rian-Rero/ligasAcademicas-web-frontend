3b8f2ec6eaa9a60437e66aee2cc978896fe656c0
larastrutz <larastrutz@hotmail.com>
2026-05-01 19:00:26 -0300

    feat: creating home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,165 @@
+import { FiCalendar } from 'react-icons/fi';
+import { IoArrowForward } from 'react-icons/io5';
+import { LiaUniversitySolid, LiaIdBadge } from 'react-icons/lia';
+import { PiUserList, PiCertificateBold } from 'react-icons/pi';
+import {
+  TbSchool,
+  TbUsersGroup,
+  TbCertificate,
+  TbHierarchy2,
+  TbCalendarClock,
+} from 'react-icons/tb';
+import { useNavigate } from 'react-router-dom';
+
+import {
+  Container,
+  MainBlock,
+  OverlayContent,
+  HeroTitle,
+  HeroSubtitle,
+  CenteredButton,
+  SystemVisualization,
+  SystemImage,
+  FloatingTag,
+  FeaturesGrid,
+  FeatureCard,
+  FeatureIcon,
+  FeatureTextContent,
+  FeatureTitle,
+  FeatureDescription,
+} from './Styles';
+import coreSystemImage from '../../assets/coreSystem.png';
+
 export default function Home() {
-  return <p>Home</p>;
+  const navigate = useNavigate();
+
+  const handleAccessSystem = () => {
+    navigate('/login');
+  };
+
+  return (
+    <Container>
+      <MainBlock>
+        <SystemVisualization>
+          <SystemImage
+            src={coreSystemImage}
+            alt="Visualização do Core System"
+          />
+
+          <OverlayContent>
+            <HeroTitle>
+              GERENCIAMENTO CENTRALIZADO
+              <br />
+              PARA LIGAS ACADÊMICAS
+            </HeroTitle>
+            <HeroSubtitle>
+              O SGLA otimiza e unifica o ecossistema universitário, da gestão de
+              ligas e subequipes à emissão automatizada de certificados.
+            </HeroSubtitle>
+
+            <CenteredButton onClick={handleAccessSystem}>
+              Acessar sistema
+              <IoArrowForward size={18} />
+            </CenteredButton>
+          </OverlayContent>
+
+          <FloatingTag $top="44%" $left="8%" $color="blue">
+            <span>
+              <LiaUniversitySolid />
+            </span>
+            Universidade
+          </FloatingTag>
+
+          <FloatingTag $top="45%" $left="28.1%" $color="blue">
+            <span>
+              <TbSchool />
+            </span>
+            Liga
+            <br />
+            Acadêmica
+          </FloatingTag>
+
+          <FloatingTag $top="70%" $left="12%" $color="blue">
+            <span>
+              <LiaIdBadge />
+            </span>
+            Departamentos
+            <br />
+            Internos
+          </FloatingTag>
+
+          <FloatingTag $top="44%" $right="-5%" $color="orange">
+            <span>
+              <TbCertificate />
+            </span>
+            Certificados
+          </FloatingTag>
+
+          <FloatingTag $top="44%" $right="15.72%" $color="orange">
+            <span role="img" aria-label="Eventos">
+              <FiCalendar />
+            </span>
+            Eventos
+          </FloatingTag>
+
+          <FloatingTag $top="70%" $right="0%" $color="orange">
+            <span role="img" aria-label="Membros">
+              <TbUsersGroup />
+            </span>
+            Membros
+          </FloatingTag>
+        </SystemVisualization>
+      </MainBlock>
+
+      <FeaturesGrid>
+        <FeatureCard>
+          <FeatureIcon>
+            <TbHierarchy2 />
+          </FeatureIcon>
+          <FeatureTextContent>
+            <FeatureTitle>HIERARQUIA SIMPLIFICADA</FeatureTitle>
+            <FeatureDescription>
+              Gerencie universidades, ligas e departamentos.
+            </FeatureDescription>
+          </FeatureTextContent>
+        </FeatureCard>
+
+        <FeatureCard>
+          <FeatureIcon>
+            <PiUserList />
+          </FeatureIcon>
+          <FeatureTextContent>
+            <FeatureTitle>ORGANIZAÇÃO DE MEMBROS</FeatureTitle>
+            <FeatureDescription>
+              Alocações, cargos e permissões.
+            </FeatureDescription>
+          </FeatureTextContent>
+        </FeatureCard>
+
+        <FeatureCard>
+          <FeatureIcon>
+            <TbCalendarClock />
+          </FeatureIcon>
+          <FeatureTextContent>
+            <FeatureTitle>EVENTOS E PRESENÇA</FeatureTitle>
+            <FeatureDescription>
+              Agendamento, locais e Dashboards de engajamento.
+            </FeatureDescription>
+          </FeatureTextContent>
+        </FeatureCard>
+
+        <FeatureCard>
+          <FeatureIcon>
+            <PiCertificateBold />
+          </FeatureIcon>
+          <FeatureTextContent>
+            <FeatureTitle>CERTIFICADOS AUTOMÁTICOS</FeatureTitle>
+            <FeatureDescription>
+              Geração, assinatura e envio personalizados.
+            </FeatureDescription>
+          </FeatureTextContent>
+        </FeatureCard>
+      </FeaturesGrid>
+    </Container>
+  );
 }
